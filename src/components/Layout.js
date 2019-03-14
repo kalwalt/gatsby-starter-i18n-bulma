5773a80d0013713cd1971fc93db50ce6a6702a53
kalwalt <info@kalwaltart.it>
2019-03-14 12:09:29 +0100

    adding startPath function but not working properly

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,22 +23,59 @@ const getIdUrl = (id, langKey) => {
   console.log("inside the getter");
   switch (langKey) {
     //we need the inverse of the current page...
-    case 'en': return articleId[id][1];
-    case 'it': return articleId[id][0];
+    case 'en': return articleId[id][0];
+    case 'it': return articleId[id][1];
     default: return null;
   }
 };
 
-const setLangsMenu = ( langsMenu, id, langKey) => {
+const startPath = (langKey, langsMenu, basename, url) => {
+  const lengthLangKey = langKey.length;
+  console.log("length of the langKey:");
+  console.log(lengthLangKey);
+  var indx;
+  switch (langKey) {
+    case 'en': return indx = langsMenu[0].link.indexOf(basename);
+    case 'it': return indx = langsMenu[1].link.indexOf(basename);
+    default: return null;
+    console.log("indx inside startPath:");
+    console.log(indx);
+  }
+  console.log("indx inside startPath:");
+  console.log(indx);
+  indx = 9;
+  console.log("url:");
+  console.log(url);
+  const basePath = url.slice(lengthLangKey + 2, indx);
+  console.log("basePath is:");
+  console.log(basePath);
+  //var basePath = '/blog'
+  return basePath;
+};
+
+const setLangsMenu = ( langsMenu, id, langKey, basePath) => {
   console.log("inside the setter");
 
-  //switch (langKey) {
+  switch (langKey) {
     //we need the inverse of the current page...
     //case 'en': return this.langsMenu[0].link = "/it/blog/" + getIdUrl(id_article, langKey);
-    return langsMenu[1].link.replace(getSlugAndLang('any', langsMenu[1].link), getIdUrl(id, langKey));
+    case 'en': return langsMenu[0].link = `/it/${basePath}` + getIdUrl(id, langKey);
+    //return langsMenu[1].link.replace(getSlugAndLang('any', langsMenu[1].link), getIdUrl(id, langKey));
     //case 'it': return this.langsMenu[0].link = "/en/blog/" + getIdUrl(id_article, langKey);
-    //default: return null;
-  //}
+    case 'it': return langsMenu[1].link = `/en/${basePath}` + getIdUrl(id, langKey);
+    default: return null;
+  }
+};
+
+const setLangsMenu2 = ( langsMenu, id, basePath) => {
+  console.log("inside the setter");
+  var eng = langsMenu[0].link;
+  var ita = langsMenu[1].link;
+  eng = `/en/${basePath}/` + getIdUrl(id, 'en');
+  ita = `/it/${basePath}/` + getIdUrl(id, 'it');
+
+  return [eng, ita];
+
 };
 
 // add concatenated locale data
@@ -59,8 +96,8 @@ class TemplateWrapper extends Component {
     console.log("language is: ");
     console.log(this.langKey);
     this.langsMenu = getLangs(langs, this.langKey, getUrlForLang(this.homeLink, url));
-    console.log("urlForLang is: ");
-    console.log(getUrlForLang(this.homeLink, url, this.langKey));
+    //console.log("urlForLang is: ");
+    //console.log(getUrlForLang(this.homeLink, url, this.langKey));
     console.log("langsMenu is: ");
     console.log(this.langsMenu);
     const id_article = data.markdownRemark.frontmatter.id;
@@ -86,9 +123,14 @@ class TemplateWrapper extends Component {
     //var slug = getSlugAndLang(options, this.langsMenu[0].link);
     var slug = getSlugAndLang(options, '/src/pages/blog/2019-03-11-notizie-dal-pianeta-arte.it.md');
     console.log(slug);
+    var Path = startPath(this.langKey, this.langsMenu, basename, url)
+    console.log("path is:");
+    //Path = '/blog'
+    console.log(Path);
 
     //this.langsMenu[0].link = "/en/blog/" + getIdUrl(id_article, this.langKey);
-    setLangsMenu( this.langsMenu, id_article, this.langKey);
+    var out = setLangsMenu2( this.langsMenu, id_article, Path);
+    console.log(out);
     // get the appropriate message file based on langKey
     // at the moment this assumes that langKey will provide us
     // with the appropriate language code
