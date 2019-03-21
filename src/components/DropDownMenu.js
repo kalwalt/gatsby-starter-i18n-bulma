a981d11f863672141b6dc8df5b185a38275443ed
kalwalt <info@kalwaltart.it>
2019-03-21 13:20:16 +0100

    now every menu tab has its own link

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -3,10 +3,13 @@ import { Link } from 'gatsby'
 import { FaImage } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import select from '../components/utils'
+import menu from '../data/artworksMenu'
 
 const DropDownMenu = (props) => {
 
   const keys = props.keys;
+  const sel = select(props.langKey);
   return (
     <div className="navbar-item has-dropdown is-hoverable">
        <Link className="navbar-link" to={props.base}>
@@ -15,7 +18,7 @@ const DropDownMenu = (props) => {
        </Link>
        <div className="navbar-dropdown is-hidden-mobile is-boxed">
         {keys &&( keys.map(( message ) => (
-         <a className="navbar-item" href="#">
+         <a className="navbar-item" href={"/" + props.langKey + "/" + menu[message][sel] + "/"}>
            <FormattedMessage id={message} />
          </a>
        )))}
---
+++ src/data/artworksMenu.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'painting': ['artworks/painting', 'opere/pittura'],
+  'sculpture': ['artworks/sculpture', 'opere/scultura'],
+  'performance': ['artworks/performance', 'opere/performance'],
+  'interactivity': ['artworks/interactivity', 'opere/interattività'],
+};
