8093e664aa188aa2849eb2a765bdfe0d600fafaa
kalwalt <info@kalwaltart.it>
2019-03-26 14:13:39 +0100

    better tabulation

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,36 +68,35 @@ module.exports = {
     },
     "gatsby-transformer-javascript-frontmatter",
     'gatsby-plugin-sharp',
-'gatsby-transformer-sharp',
-{
-  resolve: 'gatsby-transformer-remark',
-  options: {
-    plugins: [
-      {
-        resolve: 'gatsby-remark-relative-images',
-        options: {
-          name: 'uploads',
-        },
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+             options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
+          },
+        ],
       },
-      {
-        resolve: 'gatsby-remark-images',
-         options: {
-          // It's important to specify the maxWidth (in pixels) of
-          // the content container as this plugin uses this as the
-          // base for generating different widths of each image.
-          maxWidth: 2048,
-        },
-      },
-      {
-        resolve: 'gatsby-remark-copy-linked-files',
-        options: {
-          destinationDir: 'static',
-        },
-      },
-    ],
-  },
-},
-
+    },
     {
       resolve:'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
