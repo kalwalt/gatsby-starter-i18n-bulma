9fbfe02689966bb5a9f3133773914c1661c13c3e
kalwalt <info@kalwaltart.it>
2019-03-28 18:33:27 +0100

    removed gatsby-plugin-typography

--- src/templates/artworks.js
+++ src/templates/artworks.js
@@ -2,7 +2,6 @@ import React from "react"
 import * as PropTypes from "prop-types"
 import { Link, graphql } from 'gatsby'
 import Img from "gatsby-image"
-import { rhythm } from "../utils/typography"
 import Layout from "../components/Layout"
 import Content, { HTMLContent } from "../components/Content"
 import Features from '../components/Features'
