07dfaff70514234b30755d598461105435bbce74
kalwalt <github@kalwaltart.it>
2019-04-30 23:28:24 +0200

    again deleting tagSlugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,10 +29,7 @@ exports.createPages = ({ actions, graphql }) => {
             id
             fields {
               slug
-              tagSlugs {
-                tag
-                link
-              }
+            
             }
             frontmatter {
               id
@@ -80,7 +77,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
-      name: [`slug`],
+      name: `slug`,
       node,
       value,
     })
