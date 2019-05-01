41cde0f09ff7e02d0841b2a59504703e59712e3c
kalwalt <github@kalwaltart.it>
2019-05-01 23:34:49 +0200

    resolving react-dom warning (again!)

--- gatsby-node.js
+++ gatsby-node.js
@@ -83,3 +83,13 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
+}
