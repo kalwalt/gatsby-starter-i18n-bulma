8b8d6d8bfaea323aabc05110f933e9cf5cb72bb7
kalwalt <github@kalwaltart.it>
2019-05-06 14:08:10 +0200

    this solve the issue about line 42 see the comment above

--- src/components/Masonry/RenderImages.js
+++ src/components/Masonry/RenderImages.js
@@ -29,17 +29,15 @@ const RenderImage = ({
     <div
       style={{ margin, height: photo.height, width: photo.width, borderRadius: "5px", ...cont }}
     >
+      <a href={photo.link} style={{ position: "absolute", left: '2rem', top: '2rem', zIndex: "4" }} className="tag is-primary is-large">
+        <p className="has-text-centered has-text-light is-large">
+        {photo.title + ': '} <FormattedMessage id="find-out-more"/>
+      </p>
+    </a>
       <img
-        style={{}}
+        style={{ overflow: 'hidden'}}
         {...photo}
-        onClick={e => onClick(e, { index, photo })}
       />
-      //<style>{`.not-selected:hover{outline:2px solid #06befa}`}</style>
-        <a href={photo.link} style={{ position: "absolute", zIndex: "4" }} className="tag is-primary is-large">
-          <p className="has-text-centered has-text-light is-large">
-          {photo.title + ': '} <FormattedMessage id="find-out-more"/>
-        </p>
-      </a>
     </div>
   );
 };
