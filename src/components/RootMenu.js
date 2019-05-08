934a9553e4e590592ff92f6fefd514cc13225fc1
kalwalt <github@kalwaltart.it>
2019-05-08 15:25:59 +0200

    another step for the nested menu

--- src/components/RootMenu.js
+++ src/components/RootMenu.js
@@ -19,6 +19,9 @@ const RootMenu = ( {props} ) => {
           <FormattedMessage id="artworks"/>
         </a>
         <div className="navbar-dropdown">
+          <a className="navbar-item">
+            <FormattedMessage id="portfolio"/>
+          </a>
           <div className="nested navbar-item dropdown">
             <div className="dropdown-trigger">
               <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
@@ -36,10 +39,41 @@ const RootMenu = ( {props} ) => {
                baseName={basename}
                switches={keys}
                links={menu}
-               />      
+               />
            </div>
           </div>
         </div>
+        <div className="nested navbar-item dropdown">
+          <div className="dropdown-trigger">
+            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+              <FormattedMessage id="sculpture"/>
+              <span className="icon is-small">
+                <FaAngleRight  aria-hidden="true"/>
+              </span>
+            </button>
+         </div>
+          <div className="dropdown-menu" id="dropdown-menu" role="menu">
+             <div className="dropdown-content">
+             <Dropdown
+             langKey={langKey}
+             base={base}
+             baseName={basename}
+             switches={keys}
+             links={menu}
+             />
+         </div>
+        </div>
+      </div>
+        <a className="navbar-item">another one</a>
+          <a className="navbar-item">
+          Other dropdown item
+        </a>
+                  <a href="#" className="navbar-item is-active">
+          Active dropdown item
+        </a>
+                  <a href="#" className="navbar-item">
+          Other dropdown item
+        </a>
         </div>
       </div>
     )
