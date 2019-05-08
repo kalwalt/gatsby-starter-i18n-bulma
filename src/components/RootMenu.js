84f4ef1c219c47c74379d861384fc1a334e64332
kalwalt <github@kalwaltart.it>
2019-05-08 15:39:03 +0200

    adding Link and reformatting the structrue

--- src/components/RootMenu.js
+++ src/components/RootMenu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import Dropdown from '../components/DropDownMenu'
 import { FormattedMessage } from 'react-intl';
@@ -10,18 +11,17 @@ const RootMenu = ( {props} ) => {
     const keys = [ 'painting', 'sculpture', 'performance' ];
     const base = '/en/test/';
     const basename = 'artworks'
-
     console.log(keys);
     return(
       <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">
+        <Link className="navbar-link">
           <FaImage className="menu-names" />
           <FormattedMessage id="artworks"/>
-        </a>
+        </Link>
         <div className="navbar-dropdown">
-          <a className="navbar-item">
+          <Link className="navbar-item">
             <FormattedMessage id="portfolio"/>
-          </a>
+          </Link>
           <div className="nested navbar-item dropdown">
             <div className="dropdown-trigger">
               <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
@@ -64,18 +64,50 @@ const RootMenu = ( {props} ) => {
          </div>
         </div>
       </div>
-        <a className="navbar-item">another one</a>
-          <a className="navbar-item">
-          Other dropdown item
-        </a>
-                  <a href="#" className="navbar-item is-active">
-          Active dropdown item
-        </a>
-                  <a href="#" className="navbar-item">
-          Other dropdown item
-        </a>
-        </div>
+      <div className="nested navbar-item dropdown">
+        <div className="dropdown-trigger">
+          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <FormattedMessage id="performance"/>
+            <span className="icon is-small">
+              <FaAngleRight  aria-hidden="true"/>
+            </span>
+          </button>
+       </div>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+           <div className="dropdown-content">
+           <Dropdown
+           langKey={langKey}
+           base={base}
+           baseName={basename}
+           switches={keys}
+           links={menu}
+           />
+       </div>
       </div>
+    </div>
+    <div className="nested navbar-item dropdown">
+      <div className="dropdown-trigger">
+        <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+          <FormattedMessage id="interactivity"/>
+          <span className="icon is-small">
+            <FaAngleRight  aria-hidden="true"/>
+          </span>
+        </button>
+     </div>
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+         <div className="dropdown-content">
+         <Dropdown
+         langKey={langKey}
+         base={base}
+         baseName={basename}
+         switches={keys}
+         links={menu}
+         />
+     </div>
+    </div>
+  </div>
+</div>
+</div>
     )
 }
 
