35d5851aa2fb2cda7aef281099811c82f3060772
kalwalt <github@kalwaltart.it>
2021-03-09 19:16:05 +0100

    temporary solution for PreviewCompatibileImage

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { GatsbyImage } from "gatsby-plugin-image";
 
 const PreviewCompatibleImage = ({ imageInfo }) => {
-  const imageStyle = { borderRadius: '5px' }
+  const imageStyle = { borderRadius: '5px', display: 'flex' }
   const { alt = '', childImageSharp, image } = imageInfo
 
   if (!!image && !!image.childImageSharp) {
