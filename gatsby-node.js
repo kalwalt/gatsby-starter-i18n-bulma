280cc2b080ee0a7a1ac8e6bfc058aaa4a23d78c8
kalwalt <info@kalwaltart.it>
2022-02-08 18:28:40 +0100

    removing hot-loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -71,13 +71,3 @@ exports.createPages = ({ actions, graphql }) => {
     });
   });
 };
-
-exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
-  const config = getConfig();
-  if (stage.startsWith('develop') && config.resolve) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react-dom': '@hot-loader/react-dom',
-    };
-  }
-};
