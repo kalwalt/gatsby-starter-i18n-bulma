111c4f5b349249b7abc19c77485e99f01ab3153d
kalwalt <github@kalwaltart.it>
2022-02-28 23:37:07 +0100

    adding IntlProvider to solve react-intl issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,22 @@
 import * as React from "react";
 import Main from '../components/Main';
+import { IntlProvider } from 'react-intl';
 import "./all.sass";
 
 const TemplateWrapper = ({ children, className }) => {
 
   return (
+    <IntlProvider
+        locale={'it'}
+        messages={'m'}
+        textComponent={React.Fragment}
+      >
     <div>
       <Main  key="app-main" className='content'>
             {children}
         </Main>  
     </div>
+    </IntlProvider>
   );
 };
 
