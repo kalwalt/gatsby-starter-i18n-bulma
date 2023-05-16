059f72b8c79f61b27a6f6b6dfcfd62c3d2d68919
Walter Perdan <github@kalwaltart.it>
2023-05-16 15:29:50 +0200

    removing preconnect

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -142,7 +142,6 @@ export default HomePage;
 
 export const Head = () => (
   <>
-    <link href='//cdnjs.cloudflare.com/' rel='preconnect' crossOrigin="true" />
     <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
   </>
